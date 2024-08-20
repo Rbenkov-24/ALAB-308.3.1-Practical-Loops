9e0bf553c29f6fb5c4f2d87dd679044af755671b
Rbenkov-24 <benkovrj@gmail.com>
2024-08-20 17:48:09 -0400

    First commit fizz buzz

---
+++ index.js
@@ -0,0 +1,17 @@
+//------------------Part 1: Fizz Buzz------------
+//order of the conditions matter
+
+for  (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('Fizz Buzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+
+//------------------Part 2: Prime Time-----------
+//------------------Part 3: Feeling Loopy--------
