35e951eb87d4c716efc7876541982755d5f6c6c5
Rbenkov-24 <benkovrj@gmail.com>
2024-08-21 00:18:54 -0400

    part 3 feeling loopy

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // Loop through all numbers from 1 to 100.
 for (let i = 1; i <= 100; i++) {
   //If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-  if (i % 3 === 0 && i % 5 === 0) { //the most specific conditions must be checked first to avoid being overridden by the less specific conditions.
+  if (i % 3 === 0 && i % 5 === 0) {
+    //the most specific conditions must be checked first to avoid being overridden by the less specific conditions.
     console.log("Fizz Buzz");
     //If a number is divisible by 3, log “Fizz.”
   } else if (i % 3 === 0) {
@@ -31,7 +32,7 @@ while (true) {
   if (n === 2) {
     isPrime = true;
   } else {
-    //check if n is divisible 
+    //check if n is divisible
     for (let i = 2; i < n; i++) {
       if (n % i === 0) {
         isPrime = false;
@@ -48,3 +49,50 @@ while (true) {
 }
 
 //------------------Part 3: Feeling Loopy--------
+
+//function to process CSV data
+function processCSV(csvString) {
+  let row = [];
+  let cell = "";
+  let rows = [];
+
+  //Loop through the characters of a given CSV string.
+  for (let i = 0; i < csvString.length; i++) {
+    let char = csvString[i];
+
+    if (char === ",") {
+      //Store each “cell” of data in a variable.
+      row.push(cell);
+      cell = "";
+    } else if (char === "\n") {
+      //When you encounter the “\r\n” sequence, move to the next "row"
+      row.push(cell);
+      rows.push(row);
+      row = [];
+      cell = "";
+    } else {
+      cell += char;
+    }
+  }
+
+  if (cell || row.length) {
+    row.push(cell);
+    rows.push(row);
+  }
+
+  for (let i = 0; i < rows.length; i++) {
+    console.log(rows[i].join(","));
+  }
+}
+
+let csvData1 =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let csvData2 =
+  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116, 0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.20 4,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+console.log("Processing csvData1:");
+processCSV(csvData1);
+
+console.log("Processing csvData2:");
+processCSV(csvData2);
